23794743588e6a5d3c9d77d33bf0bb2649877627
ivan9rx <ivansantos.oficial2010@gmail.com>
2024-05-18 11:30:02 -0300

    atualizando

--- models/db.js
+++ models/db.js
@@ -1,8 +1,8 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize('agendamentos', 'root', '', {
-  host: 'localhost',
+const sequelize = new Sequelize('afs', 'avnadmin', 'AVNS_Yy7iU-jbI_H-KGiejIB', {
+  host: 'clinica-database-clinica-odontologica.a.aivencloud.com',
   dialect: 'mysql',
-  port: 3306
+  port: 20893
 });
 
 sequelize
